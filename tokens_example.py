bc4a1afbcb08c9ba0a02b3552c4061ba1fbf0cb1
playnwin <playnwinplayer@gmail.com>
2016-09-15 00:55:41 -0500

    Initial Commit

---
+++ tokens_example.py
@@ -0,0 +1,4 @@
+app_key = '' # Get this from the Reddit script access page
+app_secret = '' # Get this from the Reddit script access page
+access_token = '' # Get this from the first setup_tokens.py section
+refresh_token = '' # Get this from the second setup_tokens.py section
